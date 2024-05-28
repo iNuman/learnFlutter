4ead645a2d7ad13d5657f4cd0bbc5d0886beccf8
Numan Ali <flopy6663@gmail.com>
2024-05-28 20:44:22 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main()  => runApp(const MaterialApp(
+      home: Home()
+  ));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Appbar",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontFamily: "PoppinsBold"
+            )),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
+
+      ),
+      body: const Center(
+        child: Text("I'm Hello world",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.grey,
+                fontFamily: "PoppinsBold"
+            )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Text("OK",
+            style: TextStyle(
+            fontSize: 14.0,
+            color: Colors.white)),
+      ),
+    );
+  }
+}
+
