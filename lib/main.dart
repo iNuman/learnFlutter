e0a6a4766b95f732c3b6d42c64fdd7b3e65e9305
Numan Ali <flopy6663@gmail.com>
2024-05-28 21:01:48 +0500

    Rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main()  => runApp(const MaterialApp(
-      home: Home()
-  ));
-
+void main() => runApp(const MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -16,34 +13,41 @@ class Home extends StatelessWidget {
             style: TextStyle(
                 fontSize: 20.0,
                 color: Colors.white,
-                fontFamily: "PoppinsBold"
-            )),
+                fontFamily: "PoppinsBold")),
         centerTitle: true,
         backgroundColor: Colors.lightBlueAccent,
-
       ),
-      body: Container(
-        padding: const EdgeInsets.all(20.0),
-        color: Colors.lightGreenAccent,
-        child: const Center(
-          child: Text("I'm Hello world",
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text("I'm Hello world",
               style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 12.0,
                   color: Colors.grey,
-                  fontFamily: "PoppinsBold"
-              )
+                  fontFamily: "PoppinsBold")),
+          MaterialButton(
+            onPressed: () => {},
+            color: Colors.lightBlueAccent,
+            child: Text("I'm Button",
+                style: TextStyle(
+                    fontSize: 13.0,
+                    color: Colors.white,
+                    fontFamily: "PoppinsBold")),
           ),
-        ),
+          Container(
+            color: Colors.deepOrange,
+            padding: EdgeInsets.all(12.0),
+            child: Text("Clickie!!"),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
         backgroundColor: Colors.lightBlueAccent,
         child: const Text("OK",
-            style: TextStyle(
-            fontSize: 14.0,
-            color: Colors.white)),
+            style: TextStyle(fontSize: 14.0, color: Colors.white)),
       ),
     );
   }
 }
-
