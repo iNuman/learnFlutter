28aa43316c884d0b9d5cf6f4e7f2952a57a1e2cd
Numan Ali <flopy6663@gmail.com>
2024-06-02 16:11:56 +0500

    Asynchronous data

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 
@@ -5,20 +6,57 @@ class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
 
   @override
-  State<ChooseLocation> createState() => _State();
+  State<ChooseLocation> createState() => _ChooseLocation();
 }
 
-class _State extends State<ChooseLocation> {
+class _ChooseLocation extends State<ChooseLocation> {
+  int counter = 0;
+
+  void getData() async {
+    // simulating network request for let say username
+
+    String username =  await Future.delayed(Duration(seconds: 3), () {
+      return "Ali";
+    });
+
+    String bio =  await Future.delayed(Duration(seconds: 2), () {
+
+      return "Bio";
+    });
+
+    print('$username - $bio');
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    if (kDebugMode) {
+      print("initState function ran");
+      getData();
+    }
+  }
   @override
   Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print("build function ran");
+    }
+
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.lightBlueAccent,
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text("Choose location screen"),
+      body: ElevatedButton(
+        onPressed: (){
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
